6690c06349da5175c356d79cc9e829bba8eb85a8
PollyDrive <krvtzz@gmail.com>
2018-03-20 21:25:07 +0300

    ccc

--- markup/components/canvas/canvas.js
+++ markup/components/canvas/canvas.js
@@ -1,25 +1,24 @@
 export default class Canvas {
     init() {
-        this.drawField();
+        this.drawGame();
     }
-    drawField() {
+    drawGame() {
         let x, y;
         let box = 35,
             topPadding = box * 3,
             greenDark = '#49bb9c',
             greenLight = '#4af3c5',
             white = '#fff',
-            purple = '#79297d';
+            purple = '#79297d',
+            size = 30;
 
-
+        // статика:
         // вся доска
         const cnvs = document.getElementById('canvas');
         const ctx = cnvs.getContext('2d');
         ctx.fillStyle = '#54dab6';
         ctx.fillRect(0, 0, 630, 700);
         
-        
-
         // верхняя панель
         ctx.fillStyle = '#399e84';
         ctx.fillRect(0, 0, 630, 70);
@@ -29,34 +28,23 @@ export default class Canvas {
         ctx.fillStyle = white;
         ctx.font = '42px Arial';
         ctx.fillText(score, 2.3 * box, 1.5 * box);
+       
         let scoreApple = new Image();
         scoreApple.src = 'static/img/assets/canvas/apple.png';
         scoreApple.onload = function () {
             ctx.drawImage(scoreApple, 10, -7, 90, 90);
         };
 
-        // шахматка
-        for (x = 0; x < 16; x++) {
-            for (y = 0; y < 16; y++) {
-                if (x % 2 === y % 2) {
-                    ctx.fillStyle = greenDark;
-                    ctx.fillRect(box + (box * x), topPadding + (box * y), box, box);
-                } else {
-                    ctx.fillStyle = greenLight;
-                    ctx.fillRect(box + (box * x), topPadding + ( box * y), box, box);
-                }
-            }
-        }
-
         // еда
         let apple = new Image();
         apple.src = 'static/img/assets/canvas/apple.png';
-        let food = {
-            x: Math.floor(Math.random() * 16) * box + (box / 2),
-            y: Math.floor(Math.random() * 16) * box + (box / 2)
+        
+        let randomCoord = function () {
+            return Math.floor(Math.random() * 16 * box);
         };
-        apple.onload = function () {
-            ctx.drawImage(apple, box + food.x, topPadding + food.y, 70, 70);
+        let food = {
+            x: randomCoord(),
+            y: randomCoord()
         };
 
         // начало змеи
@@ -75,25 +63,42 @@ export default class Canvas {
                 d = 'right';
             } else if (e.keyCode === 40 && d !== 'up') {
                 d = 'down';
+            } else if (e.keyCode === 32) {
+                d = 'stop';
             }
         }
-
-        // сама змея, соббсно
-        function drawSnake() {
-            
+       
+        // перерисовка
+        function drawSnakeAndBoard() {
+            // размер только шахматки
+            ctx.fillStyle = 'transparent';
+            ctx.fillRect(box, box * 3, 560, 560);
+            // шахматка
+            for (x = 0; x < 16; x++) {
+                for (y = 0; y < 16; y++) {
+                    if (x % 2 === y % 2) {
+                        ctx.fillStyle = greenDark;
+                        ctx.fillRect(box + box * x, topPadding + box * y, box, box);
+                    } else {
+                        ctx.fillStyle = greenLight;
+                        ctx.fillRect(box + box * x, topPadding + box * y, box, box);
+                    }
+                    generateBonus(food, apple);
+                }
+            }
             for (let i = 0; i < snake.length; i++) {
-
                 ctx.fillStyle = (i === 0) ? purple : white;
                 ctx.fillRect(snake[i].x, snake[i].y, box, box);
-
                 ctx.strokeStyle = greenDark;
                 ctx.strokeRect(snake[i].x, snake[i].y, box, box);
             }
-
+            
             // позиция начала
             let snakeX = snake[0].x;
             let snakeY = snake[0].y;
 
+            // удалить последний элемент
+            snake.pop();
             // управление
             if (d === 'left') {
                 snakeX -= box;
@@ -103,10 +108,9 @@ export default class Canvas {
                 snakeY += box;
             } else if (d === 'right') {
                 snakeX += box; 
+            } else if (d === 'stop') {
+                snake.splice(0, snake.length);
             }
-            // удалить последний элемент
-            // snake.pop();
-            
             // новая позиция 
             let newHead = {
                 x: snakeX,
@@ -115,17 +119,58 @@ export default class Canvas {
             // добавить новый элемент в начало
             snake.unshift(newHead);
 
-            if (snakeX === food.x && snakeY === food.y) {
+            console.log(food.x);
+            console.log(newHead.x);
+
+            // создание яблока в пределах шахматки
+            function generateBonus(obj, img) {
+                if (obj.x < box * 2 || obj.x > box * 16) {
+                    obj.x = randomCoord();
+                    ctx.drawImage(img, obj.x, obj.y, 35, 35);
+                } else if (obj.y < topPadding || obj.y > box * 16) {
+                    obj.y = randomCoord();
+                } else {
+                    ctx.drawImage(img, obj.x, obj.y, 35, 35);
+                }
+            }
+
+            // удаление бонусов 
+            function deleteLayer(delX, delY, s) {
+                ctx.fillStyle = 'transparent';
+                ctx.clearRect(delX, delY, s, s);
+            }
+
+            // пиздец змейке
+            function gameOver() {
+                snake.splice(1, snake.length);
+                let over = 'GAME OVER';
+                ctx.fillStyle = white;
+                ctx.font = '42px Arial';
+                ctx.fillText(over, 3.5 * box, 1.5 * box);
+            }
+
+            // ограничение размера поля
+            if (newHead.y < topPadding || newHead.y > box * 16 || newHead.x < box || newHead.x > box * 16) {
+                gameOver();
+            }
+
+            // + за яблоки
+            if (newHead.x === food.x && newHead.y === food.y) {
+                deleteLayer(food.x, food.y, size);
+                snake.push(newHead);
+                generateBonus(food, apple);
+                ctx.fillStyle = greenDark;
+                deleteLayer(75, 15, 40);
                 score++;
-                food = {
-                    x: Math.floor(Math.random() * 16) * box + (box / 2),
-                    y: Math.floor(Math.random() * 16) * box + (box / 2)
-                };
+                ctx.fillStyle = white;
+                ctx.font = '42px Arial';
+                ctx.fillText(score, 2.3 * box, 1.5 * box);
+                
             } else {
-                // добавить ,не удаляя
+                //
             }
         }
-        let game = setInterval(drawSnake, 300);
+        let game = setInterval(drawSnakeAndBoard, 200);
     }
 }
 
